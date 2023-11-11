938bfef3abf7a4f7bbfaeb1b7f12622dbd701a60
tzw99556 <90042944+tzw99556@users.noreply.github.com>
2023-11-11 15:16:40 +0800

    q
    q

--- jenkins/scripts/deploy.sh
+++ jenkins/scripts/deploy.sh
@@ -6,5 +6,5 @@ sleep 1
 set +x
 
 echo 'Now...'
-echo 'Visit http://192.168.10.123/ to see your PHP application in action.'
+echo 'Visit http://192.168.56.1/ to see your PHP application in action.'
 
