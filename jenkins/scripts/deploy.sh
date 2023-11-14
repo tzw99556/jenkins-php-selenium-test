17b132443628eab967139727e24dc9a662f2b1f9
tzw99556 <90042944+tzw99556@users.noreply.github.com>
2023-11-15 00:23:16 +0800

    v
    v

--- jenkins/scripts/deploy.sh
+++ jenkins/scripts/deploy.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 
 set -x
-docker run -d -p 443:443 --name my-apache-php-app -v /home/src:/var/www/html php:7.2-apache
+docker run -d -p 443:443 --name my-apache-php-app -v /home/jenkins-php-selenium-test/src:/var/www/html php:7.2-apache
 sleep 1
 set +x
 
