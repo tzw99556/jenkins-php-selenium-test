b05d0aba35e04dfca44163a4c516cd73f32da16c
tzw99556 <90042944+tzw99556@users.noreply.github.com>
2023-11-14 23:58:29 +0800

    eferfe
    refefe

--- jenkins/scripts/deploy.sh
+++ jenkins/scripts/deploy.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 
 set -x
-docker run -d -p 80:80 --name my-apache-php-app -v c:\\Users\\zongw\\Documents\\GitHub\\jenkins-php-selenium-test\\src:/var/www/html php:7.2-apache
+docker run -d -p 443:443 --name my-apache-php-app -v /home/src:/var/www/html php:7.2-apache
 sleep 1
 set +x
 
